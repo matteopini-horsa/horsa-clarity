53e187d08ed8f19bd4af3ce63bd4cadb4bc3efce
matteo pini <matteo.pini@horsa.it>
2021-02-11 12:07:22 +0100

    herou fix

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
+const app_name = 'clarity-test'
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/app_name'));
 
 app.get('/*', function(req,res) {
 
-    res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/app_name/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
